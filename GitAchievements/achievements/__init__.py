b7a15682e59564af06fbd2697c028a1c63fc6fff
Cadyyan <cadyyan@Avalon>
2014-02-23 20:55:30 -0600

    Add blacksmith achievement

--- GitAchievements/achievements/__init__.py
+++ GitAchievements/achievements/__init__.py
@@ -6,6 +6,7 @@ from GitAchievements.achievements.base import Achievement, UsageLeveledAchieveme
 from GitAchievements.achievements.architect import ArchitectAchievement
 from GitAchievements.achievements.archivist import ArchivistAchievement
 from GitAchievements.achievements.author import AuthorAchievement
+from GitAchievements.achievements.blacksmith import BlacksmithAchievement
 from GitAchievements.achievements.carpenter import CarpenterAchievement
 from GitAchievements.achievements.inventor import InventorAchievement
 from GitAchievements.achievements.student import StudentAchievement
---
+++ GitAchievements/achievements/blacksmith.py
@@ -0,0 +1,17 @@
+"""
+Blacksmith
+	Created a branch using git checkout -b
+"""
+
+from GitAchievements.achievements import UsageLeveledAchievement
+
+class BlacksmithAchievement(UsageLeveledAchievement):
+	"""
+	Blacksmith
+		Created a branch using git checkout -b
+	"""
+
+	name        = 'Blacksmith'
+	description = 'Created a branch using git checkout -b'
+	cmd         = r'checkout.*\-b'
+
